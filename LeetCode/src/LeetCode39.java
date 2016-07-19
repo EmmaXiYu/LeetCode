e1ae410ede0226c083eb3b75959c81f475df6054
EmmaXiYu <emmaxi453@yahoo.como>
2016-07-19 17:34:55 -0500

    Medium

--- LeetCode/src/LeetCode39.java
+++ LeetCode/src/LeetCode39.java
@@ -1,24 +1,28 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class LeetCode39 {
 	  public List<List<Integer>> combinationSum(int[] candidates, int target) {
 	        List<List<Integer>> ll=new ArrayList<>();
 	        List<Integer> l=new ArrayList<>();
+	        HashMap<List<Integer> ,Integer> hm=new HashMap<>();
 	        Arrays.sort(candidates);
-	       combination_Sum(ll, candidates,target, 0,l, 0);
+	       combination_Sum(ll, candidates,target, 0,l, -1,hm);
 	            
 	        
 	        return ll;
 	    }
 	    
-	    public void combination_Sum( List<List<Integer>> ll,int[] candidates, int target, int sum,List<Integer> l, int index)
+	    public void combination_Sum( List<List<Integer>> ll,int[] candidates, int target, int sum,List<Integer> l, int index, HashMap<List<Integer> ,Integer> hm)
 	    {
 	        if(sum==target)
 	        {
 	        	 List<Integer> newList=new ArrayList<>(l);
-	         ll.add(newList);
+	        	   if(!hm.containsKey(newList))
+	         {ll.add(newList);
+	         hm.put(newList, ll.size()-1);}
 	         return;   
 	        }
 	        if(sum>target) 
@@ -26,7 +30,7 @@ public class LeetCode39 {
 	        	//sum-=l.get(l.size()-1);
 	        	return;}
 	        List<Integer> newList=new ArrayList<>(l);
-	        for(int i=index;i<candidates.length;i++)
+	        for(int i=index+1;i<candidates.length;i++)
 	        {
 	        
 	        int newSum=sum;
@@ -36,15 +40,15 @@ public class LeetCode39 {
 	        }
 	        
 	        newList.add(candidates[i]);
-	        combination_Sum(ll,candidates,  target , newSum, newList, i);
+	        combination_Sum(ll,candidates,  target , newSum, newList, i,hm);
 	        newList.remove(newList.size()-1);
 	        }
 	    }
 	    
 	    public static void main(String[] srgs){
 	    	LeetCode39 obj=new LeetCode39();
-	    	int [] l={8,7,4,3};
-	    	List<List<Integer>> ll=obj.combinationSum(l, 11);
+	    	int [] l={10,1,2,7,6,1,5};
+	    	List<List<Integer>> ll=obj.combinationSum(l, 8);
 	    	System.out.println("[");
 	    	for(int i=0;i<ll.size();i++)
 	    	{
